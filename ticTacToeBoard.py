84a3de38597663f30b67523c1c1359de902da353
DArtagnanRP <dartagnan.raoul.parent@gmail.com>
2024-04-27 22:43:37 -0600

    I begun making the board. I can now make the board appear, but am struggling to edit it.

---
+++ ticTacToeBoard.py
@@ -0,0 +1,20 @@
+"""def initial_board():
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    row1 = [board[0], board[1], board[2]]
+    row2 = [board[3], board[4], board[5]]
+    row3 = [board[6], board[7], board[8]]"""
+
+
+def display(board, row1, row2, row3):
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    row1 = [board[0], board[1], board[2]]
+    row2 = [board[3], board[4], board[5]]
+    row3 = [board[6], board[7], board[8]]
+    print(row1)
+    print(row2)
+    print(row3)
+    return board
+
+
+# "initial_board"()
+display(board=None, row1=None, row2=None, row3=None)
